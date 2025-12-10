7ab0f5bc18ba4b8ee4d2614bde99a52d6985f0f2
IA. MechMind <ia.mechmind@gmail.com>
2025-12-10 20:30:44 +0100

    Create start-servicios.sh

---
+++ start-servicios.sh
@@ -0,0 +1,15 @@
+# Iniciar servidor
+cd ~/heliopulse-observatory
+npm start
+
+# Modo desarrollo
+npm run dev
+
+# Con Docker
+docker-compose up --build
+
+# Ejecutar tests
+npm test
+
+# Sincronizar datos solares
+npm run solar-sync
